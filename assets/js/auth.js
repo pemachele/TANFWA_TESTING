2669d06f0d34f0b4507d9686fe308e414c6a69be
invict-inc <erickandpetercompany@gmail.com>
2021-12-01 11:39:18 +0300

    initial commit

---
+++ assets/js/auth.js
@@ -0,0 +1,100 @@
+const thePath = window.location.href;
+const path = thePath.substring(thePath.lastIndexOf("/") + 1);
+
+
+function auth() {
+    console.log(path)
+    if(path==="constitution.html"){
+        if (sessionStorage.getItem('token')) {
+            $('#download_con').replaceWith("<div id=\"download_con\" >\n" +
+                "                                    <a href=\"assets/img/tan/TANFWA_CONSTITUTION.pdf\">\n" +
+                "\n" +
+                "                                        <div class=\"mt10\"\n" +
+                "                                             style=\"background: #fcb444; justify-content: center; align-items: center; display: flex \">\n" +
+                "                                            Get full Pdf\n" +
+                "                                        </div>\n" +
+                "\n" +
+                "\n" +
+                "                                    </a>\n" +
+                "                                </div>")
+
+        } else {
+            $('#download_con').replaceWith(" <div id=\"download_con\" >\n" +
+                "                                    <a href=\"signin.html\">\n" +
+                "\n" +
+                "                                        <div class=\"mt10\"\n" +
+                "                                             style=\"background: #fcb444; justify-content: center; align-items: center; display: flex \">\n" +
+                "                                            SIGNIN\n" +
+                "                                        </div>\n" +
+                "\n" +
+                "\n" +
+                "                                    </a>\n" +
+                "                                </div>")
+        }
+
+    }else if ( path === "rules-and-regulations.html"){
+        if (sessionStorage.getItem('token')) {
+            $('#download_rules').replaceWith("<div id=\"download_rules\" >\n" +
+                "                                    <a href=\"assets/img/tan/RULE-AND-REGULATION.pdf\">\n" +
+                "\n" +
+                "                                        <div class=\"mt10\"\n" +
+                "                                             style=\"background: #fcb444; justify-content: center; align-items: center; display: flex \">\n" +
+                "                                            Get full Pdf\n" +
+                "                                        </div>\n" +
+                "\n" +
+                "\n" +
+                "                                    </a>\n" +
+                "                                </div>")
+
+        } else {
+            $('#download_rules').replaceWith(" <div id=\"download_rules\" >\n" +
+                "                                    <a href=\"signin.html\">\n" +
+                "\n" +
+                "                                        <div class=\"mt10\"\n" +
+                "                                             style=\"background: #fcb444; justify-content: center; align-items: center; display: flex \">\n" +
+                "                                            SIGNIN\n" +
+                "                                        </div>\n" +
+                "\n" +
+                "\n" +
+                "                                    </a>\n" +
+                "                                </div>")
+        }
+
+    }if ( path === "banking-details.html"){
+
+        if (sessionStorage.getItem('token')) {
+            $('#download_rules').replaceWith(" <div id=\"banking-details-div\" >\n" +
+                "                                <p>\n" +
+                "                                    Payments can be made through T-PESA no: 0733 000 996 or NMB BANK PLC - Account No.\n" +
+                "                                    22510063392 – MLIMANI CITY BRANCH;\n" +
+                "                                    ACCOUNT NAME: TANZANIA ACADEMIC AND NON-FICTION WRITERS ASSOCIATION\n" +
+                "                                </p>\n" +
+                "                            </div>")
+
+        } else {
+            $('#banking-details-div').replaceWith(" <div id=\"banking-details-div\" >\n" +
+                "                                    <a href=\"signin.html\">\n" +
+                "\n" +
+                "                                        <div class=\"mt10\"\n" +
+                "                                             style=\"background: #fcb444; justify-content: center; align-items: center; display: flex \">\n" +
+                "                                            SIGNIN\n" +
+                "                                        </div>\n" +
+                "\n" +
+                "\n" +
+                "                                    </a>\n" +
+                "                                </div>")
+        }
+    }
+
+}
+
+
+
+
+
+
+jQuery(document).ready(function () {
+   auth()
+
+
+});
---
+++ assets/js/signin.js
@@ -0,0 +1,85 @@
+function send_data() {
+
+
+    var k = function (t, k, e) {
+            var j = $('<div class="kt-alert kt-alert--outline alert alert-' + k + ' alert-dismissible" role="alert">\t\t\t<button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>\t\t\t<span></span>\t\t</div>');
+            t.find(".alert").remove(), j.prependTo(t), KTUtil.animateClass(n[0], "fadeIn animated"), j.find("span").html(e)
+        },
+        d = $('#login-submit'),
+
+        dataObject = {
+            code: 102,
+            api: 250,
+            data: {
+                email: $('#email').val(),
+                password: $('#password').val()
+            }
+        },
+
+        n = $('#login-form');
+
+    n.validate({
+        rules: {
+            phone_number: {required: !0},
+            password: {required: !0}
+
+        }
+    });
+    n.valid() && ($("#login-submit").prop('disabled', true) , $('#message_container').append("<div id=\"message\" style=\" padding:15px;margin: 15px; width:100% ; background:#fcb444 \" > Loading..... </div>"),
+
+
+            $.ajax(
+                {
+                    url: 'https://farnett.com/api/tanfwa',
+                    type: 'POST',
+                    data: JSON.stringify(dataObject),
+                    content: ('application/x-www-form-urlencoded; charset=UTF-8'),
+                    success: function (response) {
+                        // decode the json string to a js object
+                        var responseObject = JSON.parse(response);
+                        // get the code from the create js object using js notation
+                        var code = responseObject.code;
+                        // then do the if  for error or success
+                        if (code === 200) {
+                            console.log('yess')
+                            sessionStorage.setItem('user_details', JSON.stringify(responseObject.user_details)  );
+                            sessionStorage.setItem('token', responseObject.session_id);
+                            setTimeout(function () {
+                                d.removeClass("btn spinner spinner-white spinner-right").attr("disabled", !1), n.validate().resetForm();
+                                //var n = t.find(".kt-login__signin form");
+                                $('#message_container').replaceWith("<div id=\"message\" style=\" padding:15px;margin: 15px; width:100% ; background:lightgreen \" > " + responseObject.msg + " </div>")
+
+                            }, 1)
+                            $("#login-submit").prop('disabled', false)
+
+                        } else if (code === 300) {
+                            $('#message_container').replaceWith("<div id=\"message\" style=\" padding:15px;margin: 15px; width:100% ; background:#dd4b39 \" > " + responseObject.msg + " </div>")
+
+                            $("#login-submit").prop('disabled', false)
+                            // k(n,"danger",responseObject.msg);
+                            // d.removeClass("btn spinner spinner-white spinner-right").attr("disabled", !1);
+
+                        }else{
+                            $('#message_container').replaceWith("<div id=\"message\" style=\" padding:15px;margin: 15px; width:100% ; background:#dd4b39 \" > something went wrong </div>")
+
+                            $("#login-submit").prop('disabled', false)
+                        }
+
+                    }
+
+                }
+            )
+    )
+
+
+}
+
+
+function handlePortal() {
+    if (sessionStorage.getItem('token')) {
+        window.location = `https://tanfwa.or.tz/verify/${sessionStorage.getItem('token')}`
+
+    } else {
+        alert("Please sigin first")
+    }
+}
